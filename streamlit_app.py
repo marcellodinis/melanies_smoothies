0aa8ffd6605bff02d60819aaa863c2a9d411c3d5
Marcello Dinis <50338905+marcellodinis@users.noreply.github.com>
2025-03-25 15:07:12 +0000

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ name_on_order = st.text_input ('Customer Name', 'Customer Name')
 st.write(f'The name on your smoothie will be {name_on_order}.')
 
 
-#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
 pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
