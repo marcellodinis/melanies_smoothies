3e3a4349af1ec75275cf65c9bf27d2a4a5b01b63
Marcello Dinis <50338905+marcellodinis@users.noreply.github.com>
2025-03-25 14:33:02 +0000

    Updated reqs and app

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import requests
 from snowflake.snowpark.functions import col
 
 
@@ -41,3 +42,6 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered, {name_on_order}', icon="✅")
+
+smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/watermelon")
+st.text(smoothiefroot_response)
