be08e59c4099288053e644c9c07963aaed3a10c9
Marcello Dinis <50338905+marcellodinis@users.noreply.github.com>
2025-03-25 15:06:39 +0000

    update from df to pandas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 
@@ -18,8 +19,10 @@ name_on_order = st.text_input ('Customer Name', 'Customer Name')
 st.write(f'The name on your smoothie will be {name_on_order}.')
 
 
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
