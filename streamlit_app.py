d6b3281e7709f5371a0bccd6df41ad5d2651d3eb
Marcello Dinis <50338905+marcellodinis@users.noreply.github.com>
2025-03-25 14:26:36 +0000

    Ok

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,8 +7,9 @@ from snowflake.snowpark.functions import col
 st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write("Choose the fruits you want in your custom Smoothie!")
 
-session = cnx_session()
 cnx = st.connection("snowflake")
+session = cnx.session()
+
 
 
 
